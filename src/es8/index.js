141451d983dcbb984a68bcbf125f1c98fdb450df
Manuela Garcia Yebra <manuelagarciayebra@gmail.com>
2022-10-05 16:27:25 +0200

    async and await

--- src/es8/index.js
+++ src/es8/index.js
@@ -36,3 +36,33 @@ console.log(string.padStart(7, 'hi'));
 //los parámaetros de padStart cuantos caracteres voy a tener y cuantos son los que quiero añadir
 console.log(string.padEnd(12, ' ----'));
 //nos puede servir para presentar una lista de un menu
+
+//asyn and await
+
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    true
+      ? setTimeout(() => resolve('Hello World'), 3000)
+      : reject(new Error('Test Error'));
+  });
+};
+
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  // const hello2 = await otraFuncion();
+  console.log(hello + '1');
+};
+
+helloAsync();
+
+const anotherFunction = async () => {
+  //esta es la forma más correcta de manejar los errores
+  try {
+    const hello = await helloWorld();
+    console.log(hello + '2');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+anotherFunction();
